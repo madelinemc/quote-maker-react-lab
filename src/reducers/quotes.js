bdffe7967e021143b3544d247f5050d4e684fb71
madelinemc <madeline.coons@gmail.com>
2021-02-21 12:35:31 -0600

    commit tree c5c5fbb38d658ff215a61abe2d5dfef35e9c4743

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,35 @@
 export default (state = [], action) => {
-  return state;
+  let newState;
+
+  switch(action.type) {
+
+    case 'ADD_QUOTE':
+      return [...state, action.quote];
+
+    case 'REMOVE_QUOTE': //return a new object of state with all of the quotes that are NOT the removed one
+      return state.filter(quote => quote.id !== action.quoteId);
+
+    case 'UPVOTE_QUOTE':
+      newState = state.map(quote => {
+        if (quote.id === action.quoteId) {
+          return {...quote, votes: ++quote.votes}
+        } else {
+          return quote;
+        }
+      })
+      return newState;
+
+    case 'DOWNVOTE_QUOTE':
+      newState = state.map(quote => {
+        if (quote.id === action.quoteId && quote.votes > 0) {
+          return {...quote, votes: --quote.votes}
+        } else {
+          return quote;
+        }
+      })
+      return newState;
+
+    default: 
+      return state;
+  }
 }
